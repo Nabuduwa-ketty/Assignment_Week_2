272db18e6ca7ac572e2231294c03a18da9e9afde
Nabuduwa-ketty <nabuduwaketty@gmail.com>
2019-06-13 07:11:09 +0300

    Js Final

--- Src/billing.js
+++ Src/billing.js
@@ -1,5 +1,5 @@
 /*This is the percentage that determines the transaction charge*/
-const transactionCharge=0.025;
+const transactionCharge = 0.025;
 var amountTendered = 100000;
 BilledAmount = amountTendered-(amountTendered*transactionCharge)
 console.log("This is what the billed amount is (less transaction charges)");
